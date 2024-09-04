68e7fe8eb3b063532fc30ca98275999209634ab5
willsf2021 <willsf2021@gmail.com>
2024-09-04 11:12:47 -0300

    Update script.js
    added destination page dinamically

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+const body = document.querySelector(".body");
+const mainElement = document.querySelector("#main");
+const destinationElement = document.querySelector("#destination");
 const menuShow = document.querySelector("#hamburguer-menu-show");
 const menuClose = document.querySelector("#menu-close");
-
 const mobileMenu = document.querySelector("#mobile-menu");
+let destinationExploreButton = document.querySelectorAll(
+  ".destination-explore-button"
+);
+
+destinationExploreButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    main.style.display = "none";
+    destinationElement.style.display = "flex";
+    body.classList.add("background-destination");
+    button.style.borderColor = "white";
+  });
+});
 
 menuShow.addEventListener("click", () => {
   mobileMenu.style.display = "flex";
@@ -11,4 +25,14 @@ menuShow.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   mobileMenu.style.display = "none";
   menuShow.style.display = "block";
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    mobileMenu.style.display = "none";
+    menuShow.style.display = "none"; 
+  } else {
+   
+    menuShow.style.display = "block"; 
+  }
 });
