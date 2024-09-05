550cc2ab218d7c2910ce8dc5e31652a3d4545a7a
willsf2021 <willsf2021@gmail.com>
2024-09-05 15:34:47 -0300

    Update script.js
    separated responsabilities

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 const body = document.querySelector(".body");
-const mainElement = document.querySelector("#main");
-const destinationElement = document.querySelector("#destination");
 const menuShow = document.querySelector("#hamburguer-menu-show");
 const menuClose = document.querySelector("#menu-close");
 const mobileMenu = document.querySelector("#mobile-menu");
-let destinationExploreButton = document.querySelectorAll(
-  ".destination-explore-button"
-);
-
-destinationExploreButton.forEach((button) => {
-  button.addEventListener("click", () => {
-    main.style.display = "none";
-    destinationElement.style.display = "flex";
-    body.classList.add("background-destination");
-    button.style.borderColor = "white";
-  });
-});
 
 menuShow.addEventListener("click", () => {
   mobileMenu.style.display = "flex";
@@ -30,9 +16,8 @@ menuClose.addEventListener("click", () => {
 window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
     mobileMenu.style.display = "none";
-    menuShow.style.display = "none"; 
+    menuShow.style.display = "none";
   } else {
-   
-    menuShow.style.display = "block"; 
+    menuShow.style.display = "block";
   }
 });
